addf69b14e454747d97d6f2e9d525700c3866f88
lucamarsili <105172952+lucamarsili@users.noreply.github.com>
2024-10-06 10:57:21 +0200

    Add files via upload

---
+++ build/CMakeFiles/cosmolattice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cosmolattice.dir/src/cosmolattice.cpp.o"
+  "CMakeFiles/cosmolattice.dir/src/cosmolattice.cpp.o.d"
+  "DWZ2"
+  "DWZ2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cosmolattice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cosmolattice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cosmolattice.
+# This may be replaced when dependencies are built.
